708883b235f18f3330fc29caf34a66788390a33d
ChenE2000 <396909028@qq.com>
2021-05-13 14:56:03 +0800

    添加了回到顶部按钮和一些示例图片

--- static/js/util.js
+++ static/js/util.js
@@ -3,6 +3,13 @@ $(function() {
         showSnack("error", "Comming Soon!!!")
     })
 
+    $("#to_top").click(function() {
+        if ($(window).scrollTop() == 0) return
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1000)
+
+    })
 
 
 
